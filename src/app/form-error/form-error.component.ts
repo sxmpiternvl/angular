373fd0ad166b852028a30dbdb3dac31addf5b273
Dmitriy An <sxmpiternvl@icloud.com>
2024-04-25 01:17:14 +0500

    hz

---
+++ src/app/form-error/form-error.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-error.component.html',
+  styleUrl: './form-error.component.css'
+})
+export class FormErrorComponent {
+
+}
