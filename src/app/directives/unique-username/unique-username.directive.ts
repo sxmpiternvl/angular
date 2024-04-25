cdb4d7d87104f705174b03895b798d88d3ff094f
Dmitriy An <sxmpiternvl@icloud.com>
2024-04-26 00:20:24 +0500

    DONE???

--- src/app/directives/unique-username/unique-username.directive.ts
+++ src/app/directives/unique-username/unique-username.directive.ts
@@ -1,22 +1,22 @@
 import { Directive, Input } from '@angular/core';
 import { NG_VALIDATORS, Validator, AbstractControl, ValidatorFn } from '@angular/forms';
-import {AuthService} from "../../services/auth-service";
+import { AuthService } from "../../services/auth-service";
 
 export function usernameUniqueValidator(authService: AuthService): ValidatorFn {
-  return (control: AbstractControl)=> {
+  return (control: AbstractControl) => {
     if (!control.value) {
       return null;
     }
     return authService.isUnique(control.value) ? null : { 'usernameNotUnique': true };
   };
 }
+
 @Directive({
   selector: '[appUsernameUnique]',
   standalone: true,
-  providers: [{ provide: NG_VALIDATORS, useExisting: UsernameValidatorDirective, multi: true }]
+  providers: [{provide: NG_VALIDATORS, useExisting: UsernameValidatorDirective, multi: true}]
 })
 export class UsernameValidatorDirective implements Validator {
-  @Input('appUsernameUnique') username!: string;
   constructor(private authService: AuthService) {}
 
   validate(control: AbstractControl) {
